03bb2e94f931ba1f951e8fb3a00d845843b59c79
yusuf <yorepitan@gmail.com>
2023-03-20 03:36:53 -0700

    size 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: 4 byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: 4 byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a long long int: 8 byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: 4 bytes(s)\n",(unsigned long)sizeof(f));
+printf("size of a float: 4 byte(s)\n",(unsigned long)sizeof(f));
 return (0);
 }
