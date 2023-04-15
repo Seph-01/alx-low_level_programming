1963a12782e387011c600cd03410f505ec696439
yusuf <yorepitan@gmail.com>
2023-04-14 17:30:47 -0700

    malloc checked

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
+#ifdef MAIN.H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -8,7 +9,8 @@ int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 #endif
+
